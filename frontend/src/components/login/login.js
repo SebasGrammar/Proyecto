4f8c90b7c93708363bd8eeb0f7db0a496b19f6e0
lauravz98 <75417516+lauravz98@users.noreply.github.com>
2021-09-22 18:48:07 +0200

    login v2

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import './login.css';
+import deadpool from '../../img/deadpool-icon.png';
 
 class Login extends React.Component {
     render() {
         return (            
             <section className = "main">
                 <section className="iniciar">
+                    <img className="l-img" src={deadpool} alt="Deadpool Team Icon" />
                     <h1>Iniciar sesión</h1>
                     <p>Ingresa a tu cuenta de X - Force Team</p>
                     <form action="">
-                        <input type="text" id="username" name="username " placeholder="Usuario"/> <br></br>
-                        <input type="text" id="password" name="password" placeholder="Contraseña"/> <br></br>
-                        <input type="submit" value="Iniciar sesión"/>                    
+                        <input className="entryText" type="text" id="username" name="username " placeholder="Usuario"/> <br></br>
+                        <input className="entryText" type="text" id="password" name="password" placeholder="Contraseña"/> <br></br>
+                        <input className="botonPrincipal" type="submit" value="Iniciar sesión"/>
                     </form>
                         <p>¿Olvidaste tu contraseña?</p>
                 </section>
                 <section className="resgistrarse">
                     <p>¿No tienes una cuenta? </p>
-                    <button type="button">Regístrate </button>
+                    <button className="botonSecundario" type="button">Regístrate </button>
                 </section>
             </section>            
         )
