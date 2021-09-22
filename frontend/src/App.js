f1f1c1d989624f1fbcf9dc944f3d3e8d419078cd
lauravz98 <75417516+lauravz98@users.noreply.github.com>
2021-09-22 16:07:26 +0200

    se crea login.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import login from './components/login/login';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>
+        un titulo x
+      </h1>
+      <login></login>
     </div>
   );
 }
