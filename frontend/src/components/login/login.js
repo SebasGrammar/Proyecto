c9a6a6f82cfec263dfd594211eb58e4161a2ffe2
lauravz98 <75417516+lauravz98@users.noreply.github.com>
2021-09-22 17:41:26 +0200

    login v2

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import './login.css';
 
 class Login extends React.Component {
     render() {
-        return (
-            <div>
-                <h1>Iniciar sesión</h1>
-                <p>Ingresa a tu cuenta de X - Force Team</p>
-                
-            </div>
+        return (            
+            <section className = "main">
+                <section className="iniciar">
+                    <h1>Iniciar sesión</h1>
+                    <p>Ingresa a tu cuenta de X - Force Team</p>
+                    <form action="">
+                        <input type="text" id="username" name="username " placeholder="Usuario"/> <br></br>
+                        <input type="text" id="password" name="password" placeholder="Contraseña"/> <br></br>
+                        <input type="submit" value="Iniciar sesión"/>                    
+                    </form>
+                        <p>¿Olvidaste tu contraseña?</p>
+                </section>
+                <section className="resgistrarse">
+                    <p>¿No tienes una cuenta? </p>
+                    <button type="button">Regístrate </button>
+                </section>
+            </section>            
         )
     }
 }
