d30a3210ee388570309141b9128da2f2f31d0eb6
Sebastian <sebasgrammar@hotmail.com>
2021-09-24 15:33:21 -0500

    se incluyó información adicional

--- frontend/src/screens/usuarios/Usuarios.js
+++ frontend/src/screens/usuarios/Usuarios.js
@@ -1,5 +1,5 @@
 import './usuarios.css';
-import Navbar from '../../components/navbar/Navbar';
+import Navbar from '../../components/navbar/navbar';
 import Header from '../../components/header/Header';
 import Descripcion from '../../components/descripcion/Descripcion';
 import descripcion from '../../datasource/descripcion.json'
@@ -9,10 +9,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function Usuarios() {
+    // Object.values(descripcion[index]) turns object at [index] into an array of values.
+    // with item[0] being "titulo" & item[1] being "cuerpo"
     let arreglo = Object.values(descripcion[2])
+    
+    let title = Object.values(descripcion[2])[0]
+    let body = Object.values(descripcion[2])[1]
+    // could also be
+    // let title = arreglo[0]
+    // let body = arreglo[1]
+
+    // we can use destructuring here, too:
+    let [titulo, cuerpo] = Object.values(descripcion[2])
     return (
         <div className="pantalla">                                    
-            <Descripcion titulo={arreglo[0]} cuerpo={arreglo[1]} />
+            {/* <Descripcion titulo={arreglo[0]} cuerpo={arreglo[1]} /> */}
+            <Descripcion titulo={titulo} cuerpo={cuerpo} />
+            <Descripcion titulo={title} cuerpo={body} />
         </div>
     );
 }
