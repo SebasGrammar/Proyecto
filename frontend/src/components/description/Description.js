89369e8a2ae747658990bd9769b187eba64ef1a0
lauravz98 <75417516+lauravz98@users.noreply.github.com>
2021-10-11 17:53:39 +0200

    se organizo el componenente description para usuarios2

--- frontend/src/components/description/Description.js
+++ frontend/src/components/description/Description.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import './Description.css';
 
-
-
-export default function Description({ titulo, descripcion,textoBoton }) {
+export default function Description({ titulo, descripcion, textoBoton }) {
     return (
-
         <section className="o-container-description">
             <section className="o-description-text">
                 <h1>{titulo}</h1>
                 <p>{descripcion}</p>
             </section>
             <section className="o-btn-container">
-            <p className="o-textoo">{textoBoton}</p>
-            <button className="o-button-add">+</button>
-
-
+                <p className="o-textoo">{textoBoton}</p>
+                <button className="o-button-add">+</button>
             </section>
-
         </section>
-
     )
 }
 
