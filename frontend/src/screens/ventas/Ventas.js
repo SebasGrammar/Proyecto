13675585a6c97d0e3af48a2052a8e613593b2a56
lauravz98 <75417516+lauravz98@users.noreply.github.com>
2021-10-17 17:11:28 +0200

    pestaña venta con descripcion

--- frontend/src/screens/ventas/Ventas.js
+++ frontend/src/screens/ventas/Ventas.js
@@ -7,35 +7,8 @@ import descripcion from '../../datasource/descripcion.json'
 
 export default function Ventas() {
 
-    const [users, setUsers] = useState(null)
-    const [reloadUsers, setReloadUsers] = useState(true);
-
-    const token = localStorage.getItem("accessToken")
-
-    // const getUsers = async () => {
-    //     const users = await axios.get("http://localhost:3000/api/v1/users")
-    //     setUsers(users)
-    // }
-
-    useEffect(() => {
-        const fetchUsers = async () => {
-            const products = await axios.get("http://localhost:3000/api/v1/sell", {
-                headers: { // middleware/protect method
-                    "Authorization": token
-                }
-            })
-
-            setUsers(products.data.data)
-        }
-
-        fetchUsers()
-
-    }, [])
-
     let [titulo, cuerpo, textoBoton] = Object.values(descripcion[0])
-    if (users) {
-        console.log(users)
-        return (
+    return (
             //   <div>
             //   {users.map(user => (
             //      <h1>{user.name}</h1>
@@ -53,28 +26,5 @@ export default function Ventas() {
                     headers={["ID Usuario"]}
                     data={["1250"]} />
             </div>
-        )
-    } else {
-        return (
-            <div className="contenedor">
-                <h1>Loading...</h1>
-            </div>
-        )
-    }
-
-
-
-
-    // return (
-    //     <div>
-    //         {users.map(user => {
-    //             return (
-    //                 <div>
-    //                     <h1>{user.name}</h1>
-    //                 </div>
-    //             )
-    //         })}
-    //     </div>
-    // )
-
+        )    
 }
